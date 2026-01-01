1cfb54992ec4e31dd8e66f406736c0a94b61549c
Jack Dao <jack.dao03@gmail.com>
2025-12-31 23:13:29 -0800

    Enabled login through Google

--- backend/src/middleware/authenticateToken.js
+++ backend/src/middleware/authenticateToken.js
@@ -5,9 +5,14 @@ const authenticateToken = (req, res, next) => {
     const token = authHeader && authHeader.split(' ')[1];
 
     if (token == null) return res.sendStatus(401);
+
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
+        if (err) {
+            console.error("Token verification failed:", err.message);
+            return res.sendStatus(403);
+        }
+        
+        req.user = { userId: user.sub, ...user };
         next();
     });
 };
